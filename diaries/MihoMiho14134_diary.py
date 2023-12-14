b4a4aa7149cfc31031827b2c4e7b6f07048d37c2
MihoMiho14134 <153794136+MihoMiho14134@users.noreply.github.com>
2023-12-14 16:07:59 +0900

    miho

---
+++ diaries/MihoMiho14134_diary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+
+class MihoMiho14134_diary(AbstractDiary):
+    def get_date(self):
+        return "1111-11-11"
+
+    def get_summary(self):
+        return "こんにちは"
+
+    def get_author(self):
+        return "MihoMiho14134"
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from diaries.DiarySample import DiarySample
 from diaries.k22036_diary import k22036_diary
-
+from diaries.MihoMiho14134_diary import MihoMiho14134_diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), k22036_diary()]
+diaries = [DiarySample(), k22036_diary(),MihoMiho14134_diary()]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
