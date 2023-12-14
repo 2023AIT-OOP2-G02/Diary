94a7dd071ff73dbd449e86c5c6999590c1f0d65a
MukPan <109339477+MukPan@users.noreply.github.com>
2023-12-15 00:48:52 +0900

    add: 日記の追加

---
+++ diaries/K22005Diary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+
+class K22005Diary(AbstractDiary):
+    def get_date(self):
+        return "2023-12-15"
+
+    def get_summary(self):
+        return "リーダー頑張ります"
+
+    def get_author(self):
+        return "MukPan"
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from diaries.DiarySample import DiarySample
+from diaries.K22005Diary import K22005Diary
 from diaries.k22036_diary import k22036_diary
 from diaries.tokage28_diary import tokage28_diary
 from diaries.k22138_diary import k22138_diary
@@ -7,17 +8,17 @@ from diaries.k22028_diary import k22028_diary
 from diaries.k22009_diary import k22009_diary
 from diaries.HondaDiary import HondaDiary
 
-
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
-  DiarySample(),
-  k22036_diary(),
-  k22028_diary(),
-  HondaDiary(),
-  MihoMiho14134_diary(),
-  k22138_diary(),
-  tokage28_diary(),
-  k22009_diary()
+    DiarySample(),
+    k22036_diary(),
+    k22028_diary(),
+    HondaDiary(),
+    MihoMiho14134_diary(),
+    k22138_diary(),
+    tokage28_diary(),
+    k22009_diary(),
+    K22005Diary(),
 ]
 
 for d in diaries:
