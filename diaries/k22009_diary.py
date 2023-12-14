67870ef0c855758aa7bd6cba4e65559f409dd819
ammoil <153738287+ammoil@users.noreply.github.com>
2023-12-14 16:23:29 +0900

    日記の追加

---
+++ diaries/k22009_diary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+
+class k22009_diary(AbstractDiary):
+    def get_date(self):
+        return "2023-12-14"
+
+    def get_summary(self):
+        return "オブジェクト受けた"
+
+    def get_author(self):
+        return "k22009"
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from diaries.DiarySample import DiarySample
 from diaries.k22036_diary import k22036_diary
 from diaries.k22028_diary import k22028_diary
+from diaries.k22009_diary import k22009_diary
 
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), k22036_diary(), k22028_diary()]
+diaries = [DiarySample(), k22036_diary(), k22028_diary(), k22009_diary()]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
